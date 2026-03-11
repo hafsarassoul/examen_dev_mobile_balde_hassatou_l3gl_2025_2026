b82ac9e6fc9c9ba47f812023563a303ef0490895
Hafsa Balde <hafsatoubalde2002@gmail.com>
2026-03-11 03:14:17 +0000

    Ajout de l'implementation deloading_indicator.dart

---
+++ lib/widgets/common/loading_indicator.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class LoadingIndicator extends StatelessWidget {
+  final double size;
+  final Color? color;
+  final String? message;
+
+  const LoadingIndicator({
+    super.key,
+    this.size = 30.0,
+    this.color,
+    this.message,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: size,
+            width: size,
+            child: CircularProgressIndicator(
+              strokeWidth: 3,
+              valueColor: AlwaysStoppedAnimation<Color>(
+                color ?? Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          if (message != null) ...[
+            const SizedBox(height: 16),
+            Text(
+              message!,
+              style: TextStyle(
+                color: Colors.grey[600],
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ],
+      ),
+    );
+  }
+}
